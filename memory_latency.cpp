2a2f2504a8a7903f16b4c10ed9348cb306dbf041
Bar David <bar.david.248@gmail.com>
2024-05-16 19:40:45 +0300

    THE EXERCISE

--- memory_latency.cpp
+++ memory_latency.cpp
@@ -2,8 +2,13 @@
 
 #include "memory_latency.h"
 #include "measure.h"
+#include <stdint.h>
 
 #define GALOIS_POLYNOMIAL ((1ULL << 63) | (1ULL << 62) | (1ULL << 60) | (1ULL << 59))
+
+#define MAX_SIZE_ARG 1
+#define FACTOR_ARG 2
+#define REPEAT_ARG 3
 
 /**
  * Converts the struct timespec to time in nano-seconds.
@@ -28,10 +33,41 @@ uint64_t nanosectime(struct timespec t)
 */
 struct measurement measure_sequential_latency(uint64_t repeat, array_element_t* arr, uint64_t arr_size, uint64_t zero)
 {
-    // baseline measurement
-    for (uint64_t i = 0; i < repeat; i++) {
+    repeat = arr_size > repeat ? arr_size:repeat; // Make sure repeat >= arr_size
+    register uint64_t num=12345;
 
+    // Baseline measurement:
+    struct timespec t0;
+    timespec_get(&t0, TIME_UTC);
+    for (register uint64_t i = 0; i < repeat; i++)
+    {
+        for (register uint64_t index = 0; i < arr_size; i++)
+            num ^= index & zero;
     }
+    struct timespec t1;
+    timespec_get(&t1, TIME_UTC);
+
+    // Memory access measurement:
+    struct timespec t2;
+    timespec_get(&t2, TIME_UTC);
+    for (register uint64_t i = 0; i < repeat; i++)
+    {
+        for (register uint64_t index = 0; i < arr_size; i++)
+            num ^= arr[index] & zero;
+    }
+    struct timespec t3;
+    timespec_get(&t3, TIME_UTC);
+
+    // Calculate baseline and memory access times:
+    double baseline_per_cycle=(double)(nanosectime(t1)- nanosectime(t0))/(repeat);
+    double memory_per_cycle=(double)(nanosectime(t3)- nanosectime(t2))/(repeat);
+    struct measurement result;
+
+    result.baseline = baseline_per_cycle;
+    result.access_time = memory_per_cycle;
+    result.rnd = rnd;
+    return result;
+
 }
 
 /**
@@ -56,4 +92,21 @@ int main(int argc, char* argv[])
     const uint64_t zero = nanosectime(t_dummy)>1000000000ull?0:nanosectime(t_dummy);
 
     // Your code here
+    const uint64_t MAX_SIZE = strtoull(argv[MAX_SIZE_ARG], NULL, 10);
+    const double FACTOR = strtod(argv[FACTOR_ARG], NULL);
+    const uint64_t REPEAT = strtoull(argv[REPEAT_ARG], NULL, 10);
+
+    array_element_t *arr = calloc(MAX_SIZE, sizeof(array_element_t));
+
+    for (register uint64_t mem_size = 100; mem_size < MAX_SIZE;
+         mem_size = (uint64_t) (mem_size * FACTOR)) {
+        struct measurement rnd_meas =
+            measure_latency(REPEAT, arr, mem_size, zero);
+        struct measurement seq_meas =
+            measure_sequential_latency(REPEAT, arr, mem_size, zero);
+        double rnd_offset = rnd_meas.access_time - rnd_meas.baseline;
+        double seq_offset = seq_meas.access_time - seq_meas.baseline;
+
+        printf("%" PRIu64 ",%f,%f\n", mem_size, rnd_offset, seq_offset);
+    }
 }
