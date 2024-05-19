24ce2d83c9c6ec9a1ce70ea6e40350b2ed36cfc1
Yoav Strugo <yoav@strugo.net>
2024-05-19 21:59:10 +0300

    updated plot script

--- plot_example.py
+++ plot_example.py
@@ -2,22 +2,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv(r"C:\path\to\output.csv")
+data = pd.read_csv(r"./yoav_data.csv")
 data = data.to_numpy()
 
-l1_size = 0
-l2_size = 0
-l3_size = 0
+KiB = 1024
+MiB = 1024**2
+
+l1_size = 192 * KiB
+l2_size = 5 * MiB
+l3_size = 48 * MiB
 
 plt.plot(data[:, 0], data[:, 1], label="Random access")
 plt.plot(data[:, 0], data[:, 2], label="Sequential access")
 plt.xscale('log')
-plt.yscale('log')
-plt.axvline(x=l1_size, label="L1 (?? ?iB)", c='r')
-plt.axvline(x=l2_size, label="L2 (?? ?iB)", c='g')
-plt.axvline(x=l3_size, label="L3 (?? ?iB)", c='brown')
+plt.axvline(x=l1_size, label=f"L1 ({l1_size//KiB} KiB)", c='r')
+plt.axvline(x=l2_size, label=f"L2 ({l2_size//MiB} MiB)", c='g')
+plt.axvline(x=l3_size, label=f"L3 ({l3_size//MiB} MiB)", c='brown')
 plt.legend()
 plt.title("Latency as a function of array size")
-plt.ylabel("Latency (ns log scale)")
+plt.ylabel("Latency (ns)")
 plt.xlabel("Bytes allocated (log scale)")
-plt.show()
+plt.savefig('plot.png')
